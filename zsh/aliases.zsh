3128cb3145d8e35359f1b0d51507c5388abe98a2
Michael Kuehl <candlekeep@mini.fritz.box>
2025-10-20 13:09:29 +0200

    removed ruby and some minor stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,10 @@ alias reload!='. ~/.zshrc'
 # python
 alias python='python3'
 alias pip='pip3'
+
 #docker
 alias docker='podman'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias l='ls -la'
